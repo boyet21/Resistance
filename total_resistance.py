02791fda56d9f3967cb8c3a03d6e2aa5a8fe1bd7
boyet21 <jgalendezlu@hotmail.com>
2025-04-09 01:59:11 +0800

    Add combo,result label

--- total_resistance.py
+++ total_resistance.py
@@ -19,7 +19,9 @@ class TotalResistanceCalculator(QWidget):
         self.unit_combo.addItems(["Series","Parallel"])
 
         calculate_button = QPushButton("Calculate")
+        calculate_button.clicked.connect(self.calculate)
 
+        self.result_label = QLabel()
 
         #Add widgets to grid
         grid.addWidget(r1label,0,0)
@@ -27,9 +29,32 @@ class TotalResistanceCalculator(QWidget):
         grid.addWidget(r2label,1,0)
         grid.addWidget(self.r2_input,1,1)
         grid.addWidget(self.unit_combo,1,2)
-        grid.addWidget(calculate_button,3,0)
+        grid.addWidget(calculate_button,2,0)
+        grid.addWidget(self.result_label,2,1,1,2)
 
         self.setLayout(grid)
+    def calculate(self):
+        # Get the resistances from the input boxes
+        r1 = float(self.r1_input.text())
+        r2 = float(self.r2_input.text())
+
+        # Calculate the total resistances in series
+        total_resistance1 = r1 + r2
+
+        # Calculate the total resistance in parallel
+        total_resistance2 = (r1*r2)/(r1+r2)
+
+        # Check what the user chose in the combo
+        if self.unit_combo.currentText() == 'Series':
+            total_resistance = round(total_resistance1, 2)
+            unit = "ohms"
+
+        if self.unit_combo.currentText() == 'Parallel':
+            total_resistance = round(total_resistance2, 2)
+            unit = "ohms"
+
+        self.result_label.setText(f"The Total Resistance is: {total_resistance} {unit}")
+
 
 app = QApplication(sys.argv)
 calculator = TotalResistanceCalculator()
