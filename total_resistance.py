887d37d9f9316ae03a549fcfa72c984ea7adb2fb
boyet21 <jgalendezlu@hotmail.com>
2025-04-09 01:01:42 +0800

    Add unit combo

--- total_resistance.py
+++ total_resistance.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QGridLayout, QWidget, QLabel, \
-    QLineEdit, QPushButton
+    QLineEdit, QPushButton, QComboBox
 import sys
 
 class TotalResistanceCalculator(QWidget):
@@ -15,14 +15,19 @@ class TotalResistanceCalculator(QWidget):
         r2label = QLabel("R2")
         self.r2_input =QLineEdit()
 
+        self.unit_combo = QComboBox()
+        self.unit_combo.addItems(["Series","Parallel"])
+
         calculate_button = QPushButton("Calculate")
+
 
         #Add widgets to grid
         grid.addWidget(r1label,0,0)
         grid.addWidget(self.r1_input,0,1)
         grid.addWidget(r2label,1,0)
         grid.addWidget(self.r2_input,1,1)
-        grid.addWidget(calculate_button,2,0)
+        grid.addWidget(self.unit_combo,1,2)
+        grid.addWidget(calculate_button,3,0)
 
         self.setLayout(grid)
 
